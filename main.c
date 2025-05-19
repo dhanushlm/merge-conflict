ef743032a0d93e2ab1a996ff6159786a59a794b8
Ubuntu <ubuntu@ip-172-31-91-50.ec2.internal>
2025-05-19 18:14:14 +0000

    changes done on feature2

--- main.c
+++ main.c
@@ -5,7 +5,17 @@ Initial Function()
   //Sample Code
 }
 
-feaature2()
+feature1()
+{
+  //Sample Code for Feature 1
+}
+
+feature2()
+{
+  //Sample Code for Feature 2
+}
+
+Initial Security()
 {
   //Sample Code for Feature 2
 }
